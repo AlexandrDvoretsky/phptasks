193125b1dceb037a7f5f8f5aff582641ce0dd477
Alexandr Dvoretsky <alex12238089@mail.ru>
2019-08-22 16:44:38 +0300

    fix bugs

--- clearCache/bitrix/php_interface/init.php
+++ clearCache/bitrix/php_interface/init.php
@@ -5,16 +5,16 @@ define('NEWS_IBLOCK_ID', 1);
 
 AddEventHandler('iblock', 'OnAfterIBlockElementAdd', array('CIBlockHandler', 'OnAfterIBlockElementAddHandler'));
 AddEventHandler('iblock', 'OnAfterIBlockElementUpdate', array('CIBlockHandler', 'OnAfterIBlockElementUpdateHandler'));
-AddEventHandler('iblock', 'OnAfterIBlockElementDelete', array('CIBlockHandler', 'OnAfterIBlockElementDeleteHandler'));
+AddEventHandler('iblock', 'OnBeforeIBlockElementDelete', array('CIBlockHandler', 'OnBeforeIBlockElementDeleteHandler'));
 
 class CIBlockHandler
 {
     function OnAfterIBlockElementAddHandler(&$arFields)
     {
-        CModule::IncludeModule("iblock");
+        \Bitrix\Main\Loader::includeModule('iblock');
 
         if ($arFields["ID"] > 0) {
-            createAgent($arFields);
+            CIBlockHandler::createAgent($arFields);
         }
 
     }
@@ -25,40 +25,34 @@ class CIBlockHandler
     }
 
 
-    function OnAfterIBlockElementDeleteHandler($arFields)
+    function OnBeforeIBlockElementDeleteHandler($ID)
     {
-        $rsElements = getElements();
-        while ($arElement = $rsElements->Fetch()) {
-            createAgent($arElement);
+        CAgent::RemoveAgent("CIBlockHandler::clearCacheAgent(" . ($ID) . ");");
+    }
+
+    function clearCacheAgent($id)
+    {
+        \Bitrix\Main\Loader::includeModule('iblock');
+        $iblock = CIBlockElement::GetIBlockByID($id);
+
+        $GLOBALS['CACHE_MANAGER']->ClearByTag("iblock_id_" . $iblock);
+    }
+
+    function createAgent($arElement)
+    {
+        $res = CAgent::GetList(Array("ID" => "DESC"), array("NAME" => "CIBlockHandler::clearCacheAgent(" . ($arElement['ID']) . ");"));
+        if (!$agent = $res->fetch()) {
+            if(!empty( $arElement['ACTIVE_TO'])){
+                CAgent::AddAgent("CIBlockHandler::clearCacheAgent(" . ($arElement['ID']) . ");", "", "Y", '3600', "", "Y", $arElement['ACTIVE_TO'], $arElement['ID']);
+            }
+        } else {
+            if(!empty( $arElement['ACTIVE_TO'])) {
+                CAgent::Update($agent['ID'], array("NEXT_EXEC" => $arElement['ACTIVE_TO']));
+            }else{
+                CAgent::RemoveAgent("CIBlockHandler::clearCacheAgent(" . ($arElement['ID']) . ");");
+            }
         }
-
     }
-}
-
-
-function clearCacheAgent()
-{
-    $GLOBALS['CACHE_MANAGER']->ClearByTag("iblock_id_" . NEWS_IBLOCK_ID);
-}
-
-function createAgent($arElement)
-{
-    $res = CAgent::GetList(Array("ID" => "DESC"), array("NAME" => "clearCacheAgent(" . ($arElement['ID']) . ");"));
-    if (!$agent = $res->fetch()) {
-        CAgent::AddAgent("clearCacheAgent(" . ($arElement['ID']) . ");", "", "Y", '3600', "", "Y", $arElement['ACTIVE_TO'], $arElement['ID']);
-    } else {
-        CAgent::Update($agent['ID'], array("NEXT_EXEC" => $arElement['ACTIVE_TO']));
-    }
-}
-
-function getElements()
-{
-
-    $arFilter = array("IBLOCK_ID" => NEWS_IBLOCK_ID, "ACTIVE" => "Y");
-    $arSelect = array('ID', 'IBLOCK_ID', 'NAME', 'ACTIVE_TO');
-    $rsElements = CIBlockElement::GetList(Array('ACTIVE_TO' => 'ASC'), $arFilter, false, Array("nPageSize" => 50), $arSelect);
-
-    return $rsElements;
 }
 
 ?>
