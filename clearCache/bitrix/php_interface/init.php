23135dd973be4815c9025f90164fa7a74a59ed04
Alexandr Dvoretsky <alex12238089@mail.ru>
2019-08-23 17:20:54 +0300

    fix init.php

--- clearCache/bitrix/php_interface/init.php
+++ clearCache/bitrix/php_interface/init.php
@@ -1,49 +1,25 @@
 <?
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
-AddEventHandler('iblock', 'OnAfterIBlockElementAdd', array('CIBlockHandler', 'OnAfterIBlockElementAddHandler'));
-AddEventHandler('iblock', 'OnAfterIBlockElementUpdate', array('CIBlockHandler', 'OnAfterIBlockElementUpdateHandler'));
-AddEventHandler('iblock', 'OnBeforeIBlockElementDelete', array('CIBlockHandler', 'OnBeforeIBlockElementDeleteHandler'));
+AddEventHandler('iblock', 'OnAfterIBlockElementAdd', 'OnAfterIBlockElementAddHandler');
+AddEventHandler('iblock', 'OnAfterIBlockElementUpdate', 'OnAfterIBlockElementAddHandler');
+AddEventHandler('iblock', 'OnBeforeIBlockElementDelete', 'OnBeforeIBlockElementDeleteHandler');
 
-class CIBlockHandler
+CModule::AddAutoloadClasses(
+    '',
+    array(
+        'cclearcache' => '/bitrix/custom/cclearcache.php',
+    )
+);
+
+function OnAfterIBlockElementAddHandler($arFields)
 {
-    function OnAfterIBlockElementAddHandler(&$arFields)
-    {
-        if ($arFields["ID"] > 0) {
-            CIBlockHandler::createAgent($arFields);
-        }
-    }
+    CClearCache::checkAgent($arFields);
+}
 
-    function OnAfterIBlockElementUpdateHandler(&$arFields)
-    {
-        CIBlockHandler::OnAfterIBlockElementAddHandler($arFields);
-    }
-
-    function OnBeforeIBlockElementDeleteHandler($id)
-    {
-        $iblock = CIBlockElement::GetIBlockByID($id);
-        CAgent::RemoveAgent("CIBlockHandler::clearCacheAgent(" . ($id) . "," . ($iblock) . ");", "iblock");
-    }
-
-    function clearCacheAgent($id, $iblock_id)
-    {
-        $GLOBALS['CACHE_MANAGER']->ClearByTag("iblock_id_" . $iblock_id);
-    }
-
-    function createAgent($arElement)
-    {
-        $res = CAgent::GetList(Array("ID" => "DESC"), array("NAME" => "CIBlockHandler::clearCacheAgent(" . ($arElement['ID']) . "," . ($arElement['IBLOCK_ID']) .");"));
-        if (!$agent = $res->fetch()) {
-            if(!empty( $arElement['ACTIVE_TO'])){
-                CAgent::AddAgent("CIBlockHandler::clearCacheAgent(" . ($arElement['ID']) . "," . ($arElement['IBLOCK_ID']) . ");", "iblock", "Y", '3600', "", "Y", $arElement['ACTIVE_TO'], $arElement['ID']);
-            }
-        } else {
-            if(!empty( $arElement['ACTIVE_TO'])) {
-                CAgent::Update($agent['ID'], array("NEXT_EXEC" => $arElement['ACTIVE_TO']));
-            }else{
-                CAgent::RemoveAgent("CIBlockHandler::clearCacheAgent(" . ($arElement['ID']) . "," . ($arElement['IBLOCK_ID']) . ");", "iblock");
-            }
-        }
-    }
+function OnBeforeIBlockElementDeleteHandler($id)
+{
+    $iblock = CIBlockElement::GetIBlockByID($id);
+    CClearCache::removeAgent($id, $iblock);
 }
 ?>
