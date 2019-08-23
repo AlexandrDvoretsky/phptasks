af122f6d528e9f24fc2e3243e86a53e6ec4e0279
Alexandr Dvoretsky <alex12238089@mail.ru>
2019-08-23 17:18:08 +0300

    Create new file for class CClearCache. Fix bugs

---
+++ clearCache/bitrix/custom/cclearcache.php
@@ -0,0 +1,40 @@
+<?
+class CClearCache
+    {
+        function clearCacheAgent($id, $iblock_id)
+        {
+            $GLOBALS['CACHE_MANAGER']->ClearByTag("iblock_id_" . $iblock_id);
+        }
+
+        function checkAgent($arElement)
+        {
+            $res = CAgent::GetList(Array("ID" => "DESC"), array("NAME" => CClearCache::getAgentTitle($arElement['ID'], $arElement['IBLOCK_ID'])));
+            if (!$agent = $res->fetch()) {
+                if (!empty($arElement['ACTIVE_TO'])) {
+                    CClearCache::createAgent($arElement['ID'], $arElement['IBLOCK_ID'], $arElement['ACTIVE_TO'], $arElement['ID']);
+                }
+            } else {
+                if (!empty($arElement['ACTIVE_TO'])) {
+                    CAgent::Update($agent['ID'], array("NEXT_EXEC" => $arElement['ACTIVE_TO']));
+                } else {
+                    CClearCache::removeAgent($arElement['ID'], $arElement['IBLOCK_ID']);
+                }
+            }
+        }
+
+        function createAgent($id, $iblock_id, $next_exec, $sort)
+        {
+            CAgent::AddAgent(CClearCache::getAgentTitle($id, $iblock_id), "", "Y", '3600', "", "Y", $next_exec, $sort);
+        }
+
+        function removeAgent($id, $iblock_id)
+        {
+            CAgent::RemoveAgent(CClearCache::getAgentTitle($id, $iblock_id), "");
+        }
+
+        function getAgentTitle($id, $iblock_id)
+        {
+            return "CClearCache::clearCacheAgent(" . $id . "," . $iblock_id . ");";
+        }
+    }
+?>
