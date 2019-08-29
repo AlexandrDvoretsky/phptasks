423aa485cd76e82ba00fac2f4ccfc016a8d61dcd
Alexandr Dvoretsky <alex12238089@mail.ru>
2019-08-29 11:45:19 +0300

    remove global

--- simpleCatalogComponent/simple.catalog/templates/.default/component_epilog.php
+++ simpleCatalogComponent/simple.catalog/templates/.default/component_epilog.php
@@ -4,6 +4,5 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
 
 use \Bitrix\Main\Localization\Loc;
 
-global $APPLICATION;
 $APPLICATION->SetTitle(Loc::getMessage('PAGE_TITLE') . $arResult['ELEMENT_COUNT']);
 ?>
